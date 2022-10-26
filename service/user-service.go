b088e20c111913a0c61237c079e64c583de907d1
ashpex <ashpex@pm.me>
2022-10-26 12:32:26 +0700

    Init project

---
+++ service/user-service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/mashingan/smapping"
+	"golang-api/dto"
+	"golang-api/entity"
+	"golang-api/repository"
+	"log"
+)
+
+//UserService is a contract.....
+type UserService interface {
+	Update(user dto.UserUpdateDTO) entity.User
+	Profile(userID string) entity.User
+}
+
+type userService struct {
+	userRepository repository.UserRepository
+}
+
+//NewUserService creates a new instance of UserService
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{
+		userRepository: userRepo,
+	}
+}
+
+func (service *userService) Update(user dto.UserUpdateDTO) entity.User {
+	userToUpdate := entity.User{}
+	err := smapping.FillStruct(&userToUpdate, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatalf("Failed map %v:", err)
+	}
+	updatedUser := service.userRepository.UpdateUser(userToUpdate)
+	return updatedUser
+}
+
+func (service *userService) Profile(userID string) entity.User {
+	return service.userRepository.ProfileUser(userID)
+}
